04c04d3b342703bfca8d235075fe37b742e81de9
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-23 18:52:40 +0200

    autogen.sh

--- wspr-3.0/autogen.sh
+++ wspr-3.0/autogen.sh
@@ -6,3 +6,5 @@ aclocal && automake && autoconf && autoheader && ./configure
 cat Makefile | grep -v AC_CONFIG_MACRO_DIR > 1 && mv 1 Makefile
 make -j4
 make install
+test -e /usr/local//lib/python2.6/dist-packages/WsprMod/w.so || echo w.so not there
+
