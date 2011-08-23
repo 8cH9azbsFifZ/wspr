39aaa536a2addbea5c28490fed1bf5857aaca2ee
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-23 18:43:24 +0200

    autogen.sh

--- wspr-3.0/src/setup.py
+++ wspr-3.0/src/setup.py
@@ -3,10 +3,16 @@ from distutils.core import setup
 from distutils.file_util import copy_file
 import os
 
-version = "wspr 3.0"
+version = "@PACKAGE_NAME@ @PACKAGE_VERSION@"
 
-if __name__ == '__main__':
-	import sys
+def wspr_install(install):
+#
+# In a true python environment, w.so would be compiled from python
+# I'm doing a nasty hack here to support our hybrid build system -db
+#
+	if install == 1:
+	    os.makedirs('build/lib/WsprMod')
+	    copy_file('WsprMod/w.so', 'build/lib/WsprMod')
 	setup(name='Wspr',
 	version=version,
 	description='Wspr Python Module for Weak Signal detection',
@@ -22,4 +28,10 @@ communication under extreme weak-signal conditions.
 	      packages=['WsprMod'],
 	)
 
+if __name__ == '__main__':
+	import sys
+	if 'install' in sys.argv:
+		wspr_install(1)
+	else:
+		wspr_install(0)
 
