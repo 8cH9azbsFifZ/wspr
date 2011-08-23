b8e3b1c14fa53f2ca8013e1a4d3de36cf5a5a8b4
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-23 21:54:36 +0200

    .

--- wspr-3.0/autogen.sh
+++ wspr-3.0/autogen.sh
@@ -3,8 +3,8 @@
 git add .
 git commit -a -m autogen.sh
 aclocal && automake && autoconf && autoheader && ./configure
-cat Makefile | grep -v AC_CONFIG_MACRO_DIR > 1 && mv 1 Makefile
-make -j4
-make install
-test -e /usr/local//lib/python2.6/dist-packages/WsprMod/w.so || echo w.so not there
-
+#cat Makefile | grep -v AC_CONFIG_MACRO_DIR > 1 && mv 1 Makefile
+#make -j4
+#make install
+#test -e /usr/local//lib/python2.6/dist-packages/WsprMod/w.so || echo w.so not there
+dpgk-buildpackage
