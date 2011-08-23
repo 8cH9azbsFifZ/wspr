76757c63f05306e0f151babaf49ac6b166ec5dd3
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-23 17:30:59 +0200

    autogen.sh

--- wspr-3.0/autogen.sh
+++ wspr-3.0/autogen.sh
@@ -4,5 +4,5 @@ git add .
 git commit -a -m autogen.sh
 aclocal && automake && autoconf && autoheader && ./configure
 cat Makefile | grep -v AC_CONFIG_MACRO_DIR > 1 && mv 1 Makefile
-make
+make -j4
 make install
