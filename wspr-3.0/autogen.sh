d825850a6af0a16b0a5218147d61c2ac6ad43c9b
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-24 20:05:18 +0200

    autogen script updated

--- wspr-3.0/autogen.sh
+++ wspr-3.0/autogen.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-#autoscan -> configure.ac
-git add .
-git commit -a -m autogen.sh
-aclocal && automake && autoconf && autoheader && ./configure
-#cat Makefile | grep -v AC_CONFIG_MACRO_DIR > 1 && mv 1 Makefile
-#make -j4
-#make install
-#test -e /usr/local//lib/python2.6/dist-packages/WsprMod/w.so || echo w.so not there
-dpgk-buildpackage
+automake --add-missing
+autoconf
+./configure
