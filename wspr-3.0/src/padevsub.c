b79ce9911f9cfed90f444dc2e81538e4d7c6fd82
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-25 19:09:32 +0200

    nh6z patch

--- wspr-3.0/src/padevsub.c
+++ wspr-3.0/src/padevsub.c
@@ -11,6 +11,12 @@
 #ifdef HAVE_STRING_H
 # include <string.h>
 #endif
+#ifdef HAVE_STDLIB_H
+# include <stdlib.h>
+#endif
+#ifdef HAVE_SYS_PARAM_H
+# include <sys/param.h>
+#endif
 
 #define NUM_CHANNELS    (1)
 #define PA_SAMPLE_TYPE  paInt16
